7a443de7db3e8705b2d2629e85a9d7a7bb6e66ca
ohogb <147062435+ohogb@users.noreply.github.com>
2023-10-26 20:52:45 +0300

    stub wl_subsurface.set_sync()

--- hyu/src/wl/sub_surface.rs
+++ hyu/src/wl/sub_surface.rs
@@ -10,6 +10,14 @@ impl SubSurface {
 
 impl wl::Object for SubSurface {
 	fn handle(&mut self, client: &mut wl::Client, op: u16, params: Vec<u8>) -> Result<()> {
-		todo!()
+		match op {
+			4 => {
+				// wl_subsurface.set_sync()
+				// https://gitlab.freedesktop.org/wayland/wayland/blob/master/protocol/wayland.xml#L2849
+			}
+			_ => Err(format!("unknown op '{op}' in SubSurface"))?,
+		}
+
+		Ok(())
 	}
 }
