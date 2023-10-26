3dc71d139861ddba5a87a3e14408306f16f84d3e
ohogb <147062435+ohogb@users.noreply.github.com>
2023-10-26 20:55:32 +0300

    xdg_toplevel.set_title()

--- hyu/src/wl/xdg_toplevel.rs
+++ hyu/src/wl/xdg_toplevel.rs
@@ -2,12 +2,14 @@ use crate::{wl, Result};
 
 pub struct XdgToplevel {
 	app_id: String,
+	title: String,
 }
 
 impl XdgToplevel {
 	pub fn new() -> Self {
 		Self {
 			app_id: String::new(),
+			title: String::new(),
 		}
 	}
 }
@@ -15,6 +17,10 @@ impl XdgToplevel {
 impl wl::Object for XdgToplevel {
 	fn handle(&mut self, client: &mut wl::Client, op: u16, params: Vec<u8>) -> Result<()> {
 		match op {
+			2 => {
+				let title: String = wlm::decode::from_slice(&params)?;
+				self.title = title;
+			}
 			3 => {
 				let app_id: String = wlm::decode::from_slice(&params)?;
 				self.app_id = app_id;
