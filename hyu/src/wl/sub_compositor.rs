94777d68f9e3e4157f032a6fc1cdc8596e753059
ohogb <147062435+ohogb@users.noreply.github.com>
2023-10-26 20:50:40 +0300

    wl_subsurface, sub_compositor.get_subsurface()

--- hyu/src/wl/sub_compositor.rs
+++ hyu/src/wl/sub_compositor.rs
@@ -11,7 +11,15 @@ impl SubCompositor {
 
 impl wl::Object for SubCompositor {
 	fn handle(&mut self, client: &mut wl::Client, op: u16, params: Vec<u8>) -> Result<()> {
-		todo!()
+		match op {
+			1 => {
+				let (id, surface, parent): (u32, u32, u32) = wlm::decode::from_slice(&params)?;
+				client.push_client_object(id, wl::SubSurface::new());
+			}
+			_ => Err(format!("unknown op '{op}' in SubCompositor"))?,
+		}
+
+		Ok(())
 	}
 }
 
