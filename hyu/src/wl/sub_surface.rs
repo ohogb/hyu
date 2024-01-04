c3bf240bfb31abf61d24bb08b747bc3d3d606314
ohogb <147062435+ohogb@users.noreply.github.com>
2024-01-04 03:19:24 +0200

    stub `SubSurface::set_position()`

--- hyu/src/wl/sub_surface.rs
+++ hyu/src/wl/sub_surface.rs
@@ -9,8 +9,12 @@ impl SubSurface {
 }
 
 impl wl::Object for SubSurface {
-	fn handle(&mut self, _client: &mut wl::Client, op: u16, _params: Vec<u8>) -> Result<()> {
+	fn handle(&mut self, _client: &mut wl::Client, op: u16, params: Vec<u8>) -> Result<()> {
 		match op {
+			1 => {
+				// https://wayland.app/protocols/wayland#wl_subsurface:request:set_position
+				let (_x, _y): (u32, u32) = wlm::decode::from_slice(&params)?;
+			}
 			4 => {
 				// wl_subsurface.set_sync()
 				// https://gitlab.freedesktop.org/wayland/wayland/blob/master/protocol/wayland.xml#L2849
