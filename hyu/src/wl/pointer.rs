9b16a9310ee27fba6bd0e11c4cbb0802adcaae69
ohogb <147062435+ohogb@users.noreply.github.com>
2024-01-13 08:14:18 +0200

    stub `Pointer::release()`

--- hyu/src/wl/pointer.rs
+++ hyu/src/wl/pointer.rs
@@ -9,12 +9,14 @@ impl Pointer {
 }
 
 impl wl::Object for Pointer {
-	fn handle(
-		&mut self,
-		_client: &mut wl::Client,
-		_op: u16,
-		_params: Vec<u8>,
-	) -> crate::Result<()> {
-		todo!()
+	fn handle(&mut self, _client: &mut wl::Client, op: u16, _params: Vec<u8>) -> crate::Result<()> {
+		match op {
+			1 => {
+				// https://wayland.app/protocols/wayland#wl_pointer:request:release
+			}
+			_ => Err(format!("unknown op '{op}' in Pointer"))?,
+		}
+
+		Ok(())
 	}
 }
