7caa31cc20739fd3b230e3894a84d0c0981dd6a2
ohogb <147062435+ohogb@users.noreply.github.com>
2024-04-20 20:42:56 +0300

    implement `wl::ZwpLinuxDmabufV1::get_default_feedback()`

--- hyu/src/wl/zwp_linux_dmabuf_v1.rs
+++ hyu/src/wl/zwp_linux_dmabuf_v1.rs
@@ -13,8 +13,15 @@ impl ZwpLinuxDmabufV1 {
 impl wl::Object for ZwpLinuxDmabufV1 {
 	fn handle(&mut self, client: &mut wl::Client, op: u16, params: &[u8]) -> Result<()> {
 		match op {
+			2 => {
+				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_dmabuf_v1:request:get_default_feedback
+				let id: wl::Id<wl::ZwpLinuxDmabufFeedbackV1> = wlm::decode::from_slice(params)?;
+				client.new_object(id, wl::ZwpLinuxDmabufFeedbackV1::new(id));
+			}
 			_ => Err(format!("unknown op '{op}' in ZwpLinuxDmabufV1"))?,
 		}
+
+		Ok(())
 	}
 }
 
