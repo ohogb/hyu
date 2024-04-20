3571c3780057f16cf69187544604bc19b2c70252
ohogb <147062435+ohogb@users.noreply.github.com>
2024-04-20 21:37:54 +0300

    implement `wl::ZwpLinuxDmabufV1::create_params()`

--- hyu/src/wl/zwp_linux_dmabuf_v1.rs
+++ hyu/src/wl/zwp_linux_dmabuf_v1.rs
@@ -17,6 +17,11 @@ impl wl::Object for ZwpLinuxDmabufV1 {
 				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_dmabuf_v1:request:destroy
 				client.remove_object(self.object_id)?;
 			}
+			1 => {
+				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_dmabuf_v1:request:create_params
+				let id: wl::Id<wl::ZwpLinuxBufferParamsV1> = wlm::decode::from_slice(params)?;
+				client.new_object(id, wl::ZwpLinuxBufferParamsV1::new(id));
+			}
 			2 => {
 				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_dmabuf_v1:request:get_default_feedback
 				let id: wl::Id<wl::ZwpLinuxDmabufFeedbackV1> = wlm::decode::from_slice(params)?;
