cf7cadbe7cb135b2b681018fba470a3031fc2477
ohogb <147062435+ohogb@users.noreply.github.com>
2024-04-20 21:53:54 +0300

    implement `wl::ZwpLinuxBufferParamsV1::create_immed()`

--- hyu/src/wl/zwp_linux_buffer_params_v1.rs
+++ hyu/src/wl/zwp_linux_buffer_params_v1.rs
@@ -25,6 +25,21 @@ impl wl::Object for ZwpLinuxBufferParamsV1 {
 
 				let fd = client.received_fds.pop_front().unwrap();
 			}
+			3 => {
+				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_buffer_params_v1:request:create_immed
+				let (buffer_id, width, height, format, flags): (
+					wl::Id<wl::Buffer>,
+					i32,
+					i32,
+					u32,
+					u32,
+				) = wlm::decode::from_slice(params)?;
+
+				client.new_object(
+					buffer_id,
+					wl::Buffer::new(buffer_id, width, height, wl::BufferStorage::Dmabuf {}),
+				);
+			}
 			_ => Err(format!("unknown op '{op}' in ZwpLinuxBufferParamsV1"))?,
 		}
 
