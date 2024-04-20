fb5430b934f84cfd6159ed15a6d142e40193420c
ohogb <147062435+ohogb@users.noreply.github.com>
2024-04-20 21:40:36 +0300

    stub `wl::ZwpLinuxBufferParamsV1::add()`

--- hyu/src/wl/zwp_linux_buffer_params_v1.rs
+++ hyu/src/wl/zwp_linux_buffer_params_v1.rs
@@ -13,6 +13,18 @@ impl ZwpLinuxBufferParamsV1 {
 impl wl::Object for ZwpLinuxBufferParamsV1 {
 	fn handle(&mut self, client: &mut wl::Client, op: u16, params: &[u8]) -> Result<()> {
 		match op {
+			1 => {
+				// https://wayland.app/protocols/linux-dmabuf-v1#zwp_linux_buffer_params_v1:request:add
+				let (plane_idx, offset, stride, modifier_hi, modifier_lo): (
+					u32,
+					u32,
+					u32,
+					u32,
+					u32,
+				) = wlm::decode::from_slice(params)?;
+
+				let fd = client.received_fds.pop_front().unwrap();
+			}
 			_ => Err(format!("unknown op '{op}' in ZwpLinuxBufferParamsV1"))?,
 		}
 
