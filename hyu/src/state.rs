18babad34991aac6ef650b175467fdadbbeff073
ohogb <147062435+ohogb@users.noreply.github.com>
2024-06-25 19:27:20 +0300

    move window logic into state

--- hyu/src/state.rs
+++ hyu/src/state.rs
@@ -198,3 +198,243 @@ pub fn process_focus_changes(
 
 	Ok(())
 }
+
+pub fn on_cursor_move(cursor_position: (i32, i32)) -> Result<()> {
+	let mut clients = CLIENTS.lock().unwrap();
+	let cursor_position = <(i32, i32)>::from(cursor_position);
+	let cursor_position = Point(cursor_position.0, cursor_position.1);
+
+	for client in clients.values_mut() {
+		for seat in client.objects_mut::<wl::Seat>() {
+			seat.pointer_position = cursor_position;
+		}
+	}
+
+	let old = {
+		let mut lock = POINTER_OVER.lock().unwrap();
+		let ret = *lock;
+
+		*lock = None;
+		ret
+	};
+
+	let mut moving = None;
+
+	'outer: for (client, window) in WINDOW_STACK.lock().unwrap().iter() {
+		if POINTER_OVER.lock().unwrap().is_some() {
+			break;
+		}
+
+		let client = clients.get_mut(client).unwrap();
+
+		for seat in client.objects_mut::<wl::Seat>() {
+			if seat.moving_toplevel.is_some() {
+				moving = Some((client.fd, seat.object_id));
+				break;
+			}
+		}
+
+		if moving.is_some() {
+			break;
+		}
+
+		fn is_point_inside_area(cursor: Point, position: Point, size: Point) -> bool {
+			cursor.0 > position.0
+				&& cursor.1 > position.1
+				&& cursor.0 <= position.0 + size.0
+				&& cursor.1 <= position.1 + size.1
+		}
+
+		fn is_cursor_over_surface(
+			cursor_position: Point,
+			surface_position: Point,
+			surface: &wl::Surface,
+		) -> bool {
+			if let Some(input_region) = &surface.current_input_region {
+				for area in &input_region.areas {
+					let position = surface_position + area.0;
+
+					if is_point_inside_area(cursor_position, position, area.1) {
+						return true;
+					}
+				}
+			} else if let Some(&(size, ..)) = surface.data.as_ref() {
+				return is_point_inside_area(cursor_position, surface_position, size);
+			}
+
+			false
+		}
+
+		fn do_stuff(
+			client: &mut wl::Client,
+			toplevel: &wl::XdgToplevel,
+			surface: &wl::Surface,
+			cursor_position: Point,
+			surface_position: Point,
+		) {
+			if is_cursor_over_surface(cursor_position, surface_position, surface) {
+				*POINTER_OVER.lock().unwrap() = Some(PointerOver {
+					fd: client.fd,
+					toplevel: toplevel.object_id,
+					surface: surface.object_id,
+					position: cursor_position - surface_position,
+				});
+			}
+
+			for child in &surface.children {
+				let sub_surface = client.get_object(*child).unwrap();
+				let surface = client.get_object(sub_surface.surface).unwrap();
+
+				do_stuff(
+					client,
+					toplevel,
+					surface,
+					cursor_position,
+					surface_position + sub_surface.position,
+				);
+			}
+		}
+
+		let toplevel = client.get_object(*window).unwrap();
+		let xdg_surface = client.get_object(toplevel.surface).unwrap();
+		let surface = client.get_object(xdg_surface.surface).unwrap();
+
+		let position = toplevel.position - xdg_surface.position;
+
+		for &popup in &xdg_surface.popups {
+			let popup = client.get_object(popup).unwrap();
+			let xdg_surface = client.get_object(popup.xdg_surface).unwrap();
+			let surface = client.get_object(xdg_surface.surface).unwrap();
+
+			let position = (position - xdg_surface.position) + popup.position;
+
+			do_stuff(client, toplevel, surface, cursor_position, position);
+
+			if POINTER_OVER.lock().unwrap().is_some() {
+				break 'outer;
+			}
+		}
+
+		do_stuff(client, toplevel, surface, cursor_position, position);
+	}
+
+	if let Some((fd, seat)) = moving {
+		let client = clients.get_mut(&fd).unwrap();
+		let seat = client.get_object_mut(seat).unwrap();
+
+		if let Some((toplevel, window_start_pos, pointer_start_pos)) = &seat.moving_toplevel {
+			let toplevel = client.get_object_mut(*toplevel).unwrap();
+
+			toplevel.position = *window_start_pos + (seat.pointer_position - *pointer_start_pos);
+		}
+	}
+
+	let current = *POINTER_OVER.lock().unwrap();
+
+	if old.is_none() && current.is_none() {
+		return Ok(());
+	}
+
+	if old.map(|x| (x.fd, x.surface)) != current.map(|x| (x.fd, x.surface)) {
+		if let Some(PointerOver { fd, surface, .. }) = old {
+			let client = clients.get_mut(&fd).unwrap();
+
+			for pointer in client.objects_mut::<wl::Pointer>() {
+				pointer.leave(client, surface).unwrap();
+				pointer.frame(client).unwrap();
+			}
+		}
+
+		if let Some(PointerOver {
+			fd,
+			surface,
+			position,
+			..
+		}) = current
+		{
+			let client = clients.get_mut(&fd).unwrap();
+
+			for pointer in client.objects_mut::<wl::Pointer>() {
+				pointer.enter(client, surface, position).unwrap();
+				pointer.frame(client).unwrap();
+			}
+		}
+	} else if old.map(|x| x.position) != current.map(|x| x.position) {
+		let PointerOver { fd, position, .. } = current.unwrap();
+
+		let client = clients.get_mut(&fd).unwrap();
+
+		for pointer in client.objects_mut::<wl::Pointer>() {
+			pointer.motion(client, position).unwrap();
+			pointer.frame(client).unwrap();
+		}
+	}
+
+	Ok(())
+}
+
+pub fn on_mouse_button_left(input_state: u32) -> Result<()> {
+	let mut clients = CLIENTS.lock().unwrap();
+
+	for client in clients.values_mut() {
+		for seat in client.objects_mut::<wl::Seat>() {
+			if seat.moving_toplevel.is_some() {
+				assert!(input_state == 0);
+				seat.moving_toplevel = None;
+
+				return Ok(());
+			}
+		}
+	}
+
+	if let Some(PointerOver { fd, toplevel, .. }) = *POINTER_OVER.lock().unwrap() {
+		let client = clients.get_mut(&fd).unwrap();
+
+		for pointer in client.objects_mut::<wl::Pointer>() {
+			pointer.button(client, 0x110, input_state).unwrap();
+			pointer.frame(client).unwrap();
+		}
+
+		let Some(&topmost) = WINDOW_STACK.lock().unwrap().front() else {
+			panic!();
+		};
+
+		if topmost != (fd, toplevel) {
+			CHANGES.lock().unwrap().push(Change::Pick(fd, toplevel));
+		}
+	}
+	Ok(())
+}
+
+pub fn on_keyboard_button(code: u32, input_state: u32) -> Result<()> {
+	let mut clients = CLIENTS.lock().unwrap();
+
+	if let Some((client, _window)) = WINDOW_STACK.lock().unwrap().iter().next() {
+		let client = clients.get_mut(client).unwrap();
+
+		for keyboard in client.objects_mut::<wl::Keyboard>() {
+			if keyboard.key_states[code as usize] != (input_state != 0) {
+				keyboard.key_states[code as usize] = input_state != 0;
+				keyboard.key(client, code, input_state).unwrap();
+			}
+
+			// TODO: xkb
+			let modifier = match code {
+				42 => 1,
+				29 => 4,
+				_ => {
+					continue;
+				}
+			};
+
+			match input_state {
+				1 => keyboard.modifiers |= modifier,
+				0 => keyboard.modifiers &= !modifier,
+				_ => {}
+			}
+
+			keyboard.modifiers(client, keyboard.modifiers).unwrap();
+		}
+	}
+	Ok(())
+}
