247a566f2700de6d603d19a4a89f1dabb979e6b1
ohogb <147062435+ohogb@users.noreply.github.com>
2025-01-16 11:53:42 +0200

    clean up udev

--- hyu/src/udev/mod.rs
+++ hyu/src/udev/mod.rs
@@ -1,15 +1,24 @@
 #[link(name = "udev")]
 unsafe extern "C" {
 	fn udev_new() -> Option<Instance>;
+	fn udev_unref(instance: usize);
 }
 
 #[repr(transparent)]
 pub struct Instance {
-	ptr: std::ptr::NonNull<()>,
+	ptr: std::num::NonZeroUsize,
 }
 
 impl Instance {
 	pub fn create() -> Option<Self> {
 		unsafe { udev_new() }
+	}
+}
+
+impl Drop for Instance {
+	fn drop(&mut self) {
+		unsafe {
+			udev_unref(self.ptr.get());
+		}
 	}
 }
